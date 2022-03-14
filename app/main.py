cbd31f408e376b076d03471f9489a308d2e789af
dot <dzwu98@gmai.com>
2022-03-14 23:40:06 +0800

    [feat] Frontend design

--- app/main.py
+++ app/main.py
@@ -1,28 +1,72 @@
 import json
 import os
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict
 
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 from api import buildings
 
+app = FastAPI()
+
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+app.mount("/static/css", StaticFiles(directory="static/css"), name="css")
+app.mount("/static/js", StaticFiles(directory="static/js"), name="js")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+base_path = Path().resolve()
+
 class Item(BaseModel):
     parking_info: Dict[str, str]
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-base_path = Path().resolve()
-
 @app.get("/")
 def read_root():
-    base_path = Path().resolve()
-    with open(os.path.join(base_path, "templates", "index.html"), "r", encoding="utf8") as f:
+    with open(os.path.join(base_path, "templates", "Map.html"), "r", encoding="utf8") as f:
         html = f.read()
     return HTMLResponse(html)
+
+@app.get("/Map")
+def read_map():
+    with open(os.path.join(base_path, "templates", "Map.html"), "r", encoding="utf8") as f:
+        html = f.read()
+    return HTMLResponse(html)
+
+@app.get("/Info")
+def read_info():
+    with open(os.path.join(base_path, "templates", "Info.html"), "r", encoding="utf8") as f:
+        html = f.read()
+    return HTMLResponse(html)
+
+@app.get("/Apply")
+def read_apply():
+    with open(os.path.join(base_path, "templates", "Apply.html"), "r", encoding="utf8") as f:
+        html = f.read()
+    return HTMLResponse(html)
+
+@app.get("/apply/chart")
+def read_apply_chart():
+    with open(os.path.join(base_path, "templates", "apply", "chart.html"), "r", encoding="utf8") as f:
+        html = f.read()
+    return HTMLResponse(html)
+
+@app.get("/apply/license")
+def read_apply_license():
+    return FileResponse(os.path.join(base_path, "templates", "apply", "license.html"))
+
+@app.get("/apply/personal")
+def read_apply_personal():
+    return FileResponse(os.path.join(base_path, "templates", "apply", "personal.html"))
 
 @app.get("/buildings/{building_name}")
 def read_buildings(building_name: str):
