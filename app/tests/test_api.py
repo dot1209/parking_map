1f456e655eae4991679be978890859353e55776f
dot1209 <dzwu98@gmai.com>
2022-03-24 15:19:46 +0800

    [feat] Home page layout

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,7 +1,13 @@
 import json
 
 def test_building_api():
+    with open("app/data/building_distance.json", "r", encoding="utf8") as f:
+        building_distance_log = json.load(f)
+
+    assert(building_distance_log["醫護大樓"] == building_distance_log["醫護大樓"])    
+
+def test_parking_api():
     with open("app/data/parking_distance.json", "r", encoding="utf8") as f:
         parking_distance_log = json.load(f)
 
-    assert(parking_distance_log["醫護大樓"] == parking_distance_log["醫護大樓"])
+    assert(parking_distance_log["雲平地停"] == parking_distance_log["雲平地停"])
