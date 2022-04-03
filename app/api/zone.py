af9eaa2b96c27ce836c50a24efd91c8a9748f736
dot <dzwu98@gmail.com>
2022-04-03 22:18:03 +0800

    [fix] add aliasing when searching

--- app/api/zone.py
+++ app/api/zone.py
@@ -2,9 +2,6 @@ import json
 import os
 import pathlib
 
-def alias(name: str):
-    pass
-
 def find_zone(name: str):
     base_path = pathlib.Path().resolve()
 
@@ -13,11 +10,14 @@ def find_zone(name: str):
     with open(os.path.join(data_path, "ncku_buildings.json"), "r", encoding="utf8") as f:
         ncku_buildings = json.load(f)
 
+    with open(os.path.join(data_path, "aliasing.json"), "r", encoding="utf-8") as f:
+        aliasing_log = json.load(f)
+
     for buildings_zone, buildings in ncku_buildings.items():
         for building in buildings:
-            if building == name:
+            if building == aliasing_log[name]:
                 return buildings_zone
     return None
 
 if __name__ == "__main__":
-    print(find_zone("701台南市東區學人宿舍"))
+    print(find_zone("工設系館"))
